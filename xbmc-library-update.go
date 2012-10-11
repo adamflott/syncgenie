75bf372a6987680b2087027d5d44757a1486d0ae
Adam Flott <adam@npjh.com>
2012-10-11 10:43:00 -0400

    Use richer option handling library, add ability to use HTTP simple auth, and add support for cleaning / scanning the audio / video libraries

--- xbmc-library-update.go
+++ xbmc-library-update.go
@@ -5,46 +5,88 @@ package main
    because the standard library stinks */
 
 import (
-        "encoding/json"
-        "io/ioutil"
-        "log"
-        "net/http"
-        "os"
-        "strings"
+	"encoding/json"
+	"github.com/voxelbrain/goptions"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-        host := os.Args[1]
-        port := os.Args[2]
+	options := struct {
+		Host          string `goptions:"-h, --host, description='Host to connect to'"`
+		Port          int    `goptions:"-p, --port, description='Host port'"`
+		User          string `goptions:"-u, --user, description='HTTP simple auth username'"`
+		Password      string `goptions:"-w, --password, description='HTTP simple auth password'"`
+		goptions.Help `goptions:"-h, --help, description='Show this help'"`
 
-        address := "http://" + host + ":" + port + "/jsonrpc"
+		goptions.Verbs
+		Video struct {
+			Scan  bool `goptions:"--scan, description='Scan video library'"`
+			Clean bool `goptions:"--clean, description='Clean video library'"`
+		} `goptions:"video"`
+		Audio struct {
+			Scan  bool `goptions:"--scan, description='Scan audio library'"`
+			Clean bool `goptions:"--clean, description='Clean audio library'"`
+		} `goptions:"audio"`
+	}{
+		Host:     "127.0.0.1",
+		Port:     8080,
+		User:     "xbmc",
+		Password: "xbmc",
+	}
 
-        data, err := json.Marshal(map[string]interface{}{
-                "method": "VideoLibrary.Scan",
-                "id":     "xbmc-library-update",
-                "jsonrpc": "2.0",
-        })
+	goptions.ParseAndFail(&options)
 
-        resp, err := http.Post(address, "application/json", strings.NewReader(string(data)))
-        if err != nil {
-                log.Fatalf("Post: %v", err)
-        }
+	address := "http://" + options.Host + ":" + strconv.Itoa(options.Port) + "/jsonrpc"
 
-        defer resp.Body.Close()
+	method := "VideoLibrary.Scan"
 
-        body, err := ioutil.ReadAll(resp.Body)
+	if options.Video.Clean == true {
+		method = "VideoLibrary.Clean"
+	} else if options.Audio.Scan == true {
+		method = "AudioLibrary.Scan"
+	} else if options.Audio.Clean == true {
+		method = "AudioLibrary.Clean"
+	}
 
-        if err != nil {
-                log.Fatalf("ReadAll: %v", err)
-        }
+	data, err := json.Marshal(map[string]interface{}{
+		"method":  method,
+		"id":      "xbmc-library-update",
+		"jsonrpc": "2.0",
+	})
 
-        result := make(map[string]interface{})
+	client := &http.Client{}
 
-        err = json.Unmarshal(body, &result)
+	req, _ := http.NewRequest("POST", address, strings.NewReader(string(data)))
 
-        if result["result"] != "OK" {
-                os.Exit(1)
-        }
+	req.SetBasicAuth(options.User, options.Password)
+	req.Header.Set("Content-Type", "application/json")
 
-        os.Exit(0)
+	resp, resp_err := client.Do(req)
+
+	defer resp.Body.Close()
+
+	if resp_err != nil {
+		log.Fatalf("Post: %v", resp_err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatalf("ReadAll: %v", err)
+	}
+
+	result := make(map[string]interface{})
+
+	err = json.Unmarshal(body, &result)
+
+	if result["result"] != "OK" {
+		log.Fatalf("Result: %s\n, Error: %v", result["result"], err)
+	}
+
+	os.Exit(0)
 }
